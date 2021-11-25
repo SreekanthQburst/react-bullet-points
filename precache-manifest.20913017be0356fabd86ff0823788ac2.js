e03cfc650373f48d9591993af8799ddc04191393
Sreekanth V <sreekanth.vinod@qburst.com>
2021-11-25 13:42:47 +0530

    Updates

---
+++ precache-manifest.20913017be0356fabd86ff0823788ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3524f12ae6365b0f9b7fca1b3e549f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0caa272cea481a5ba3bb",
+    "url": "./static/css/main.99f52779.chunk.css"
+  },
+  {
+    "revision": "7e927f024e2700972477",
+    "url": "./static/js/2.505ba3fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.505ba3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0caa272cea481a5ba3bb",
+    "url": "./static/js/main.68c770ff.chunk.js"
+  },
+  {
+    "revision": "ae5da9677a1b372b28bb",
+    "url": "./static/js/runtime-main.77dcbcf0.js"
+  }
+]);
